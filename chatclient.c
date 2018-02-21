5608501719a236dcb2b16d403c8fd1b47a8fd430
Jimmy Kao <jimmymkao@gmail.com>
2018-02-20 17:16:21 -0800

    removed test file

--- chatclient.c
+++ chatclient.c
@@ -9,22 +9,25 @@
 #include <netdb.h>
 
 
-
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
 
+	
 int main(int argc, char *argv[])
 {
 
-	// Initialize variables
-	int socketFD, portNumber, charsWritten, charsRead;
+	// Declare/initialize  variables
+	int socketFD, portNumber;
 	struct sockaddr_in serverAddress;
 	struct hostent* serverHostInfo;
 	size_t bufferLength = 1024;
 	size_t handleLength = 256;
 	char *buffer = calloc(bufferLength, sizeof(char));
 	char *message = calloc(bufferLength, sizeof(char));
+	char *outputMessage = calloc(bufferLength, sizeof(char));
 	char *handle = calloc(handleLength, sizeof(char));
 	int charsTotal, charsTrans;
+	char inputChar;	
+	int msgIndex = 0;
 
 	// Check number of arguments  
 	if (argc < 3)	{ fprintf(stderr, "USAGE: %s hostname port\n", argv[0]); exit(0); }
@@ -58,18 +61,21 @@ int main(int argc, char *argv[])
 	printf("Connected to server.\n");
 
 	// Declare variables for ncurses
-	WINDOW *input, *output;
+	WINDOW *inputW, *outputW;
 
 	initscr();
 	cbreak();
 	echo();
 
-	input = newwin(1, COLS, LINES - 1 ,0);		// User inputs at bottom of screen
-	output = newwin(LINES - 1, COLS, 0, 0);		// Output displayed above
-	wmove(output, LINES - 2, 0);
-	scrollok(output, TRUE);
+	inputW = newwin(1, COLS, LINES - 1 ,0);		// User inputs at bottom of screen
+	outputW = newwin(LINES - 1, COLS, 0, 0);		// Output displayed above
+	wmove(outputW, LINES - 2, 0);
+	scrollok(outputW, TRUE);
+	keypad(inputW, true);
 
 	fd_set readfds;		// Declare read file descriptor set
+	mvwprintw(inputW, 0, 0, handle);			// Print handle to screen
+	wrefresh(inputW);	
 
 	// Enter infinite loop
 	while (1) {
@@ -82,8 +88,6 @@ int main(int argc, char *argv[])
 		FD_SET(socketFD, &readfds);				// Add socket FD
 		FD_SET(STDIN_FILENO, &readfds);			// Add stdin 
 	
-		mvwprintw(input, 0, 0, handle);			// Print handle to screen
-		wrefresh(input);	
 	
 		if(select(socketFD+1, &readfds, NULL, NULL, NULL) < 0) {
 			error("Error when calling select function.");
@@ -110,47 +114,70 @@ int main(int argc, char *argv[])
 			*(termChar+1) = '\0';
 
 			// Print message to output window
-			waddstr(output, buffer);
-			wrefresh(output);
+			waddstr(outputW, buffer);
+			wrefresh(outputW);
 		}
 
 		// Check stdin
 		if (FD_ISSET(STDIN_FILENO, &readfds)) {
 		
-			// Get input from user
-			if (wgetnstr(input, message, bufferLength) != OK) {
+			// Read character from user
+			inputChar = wgetch(inputW);
+		
+			if (inputChar == KEY_UP || inputChar == KEY_DOWN || inputChar == KEY_LEFT || inputChar == KEY_RIGHT) {
 				continue;
 			}
-			werase(input);
-			wrefresh(input);			
+			if (inputChar == KEY_BACKSPACE || inputChar == '\b' || inputChar == 8 ||
+					inputChar == KEY_DC || inputChar == 127) {
+				wdelch(inputW);
+				wrefresh(inputW);
+				if (msgIndex != 0) {
+					outputMessage[msgIndex - 1] = 0;
+					msgIndex--;
+				}
+			} else {
+				outputMessage[msgIndex] = inputChar;
+				msgIndex++;
+			}
 	
-			// Check if message is greater than 500 characters
-			if (strlen(message) > 500) {
-				waddstr(output, "Message should be less than 500 characters.\n");
-				wrefresh(output);
-				continue;
-			}
-			// If user types \quit, close socket and exit program
-			if (strcmp(message, "\\quit") == 0) {
-				close(socketFD);
-				endwin();
-				exit(0);
-			}
-					
-			strcpy(buffer, handle);		// Copy handle to buffer
-			strcat(buffer, message);	// Concatenate message to buffer
-			strcat(buffer, "\n");		// COncatenate new line
+			if (outputMessage[msgIndex-1] == '\n') {
 
-			// Send message over socket
-			while (charsTotal < strlen(buffer)) {
-				charsTrans = send(socketFD, buffer + charsTotal*sizeof(char), sizeof(buffer), 0);
-				if (charsTrans < 0) error("CLIENT: ERROR writing to socket");
-				charsTotal += charsTrans;
-			}
+					// Check if message is greater than 500 characters
+					if (strlen(outputMessage) > 500) {
+						waddstr(outputW, "Message should be less than 500 characters.\n");
+						wrefresh(outputW);
+						continue;
+					}
+					// If user types \quit, close socket and exit program
+					if (strcmp(outputMessage, "\\quit\n") == 0) {
+						close(socketFD);
+						endwin();
+						exit(0);
+					}
+							
+					strcpy(buffer, handle);		// Copy handle to buffer
+					strcat(buffer, outputMessage);	// Concatenate message to buffer
 
-			// Print message to output window
-			waddstr(output, buffer);
-			wrefresh(output);
+					// Send message over socket
+					while (charsTotal < strlen(buffer)) {
+						charsTrans = send(socketFD, buffer + charsTotal*sizeof(char), sizeof(buffer), 0);
+						if (charsTrans < 0) error("CLIENT: ERROR writing to socket");
+						charsTotal += charsTrans;
+					}
+
+					// Clear output message buffer
+					memset(outputMessage, '\0', bufferLength);
+					//Reset msgIndex
+					msgIndex = 0;
+					// Print message to output window
+					waddstr(outputW, buffer);
+					wrefresh(outputW);
+
+					// Update input window
+					werase(inputW);
+					mvwprintw(inputW, 0, 0, handle);			// Print handle to screen
+					wrefresh(inputW);
+			}
 		}
 	}	
 }
